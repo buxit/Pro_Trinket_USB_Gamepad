4b27c45df9904521b249faa0c075367fda3577d4
till busch <till@bux.at>
2016-03-14 13:17:13 +0100

    adapt for gamepads / joysticks

--- usbconfig.h
+++ usbconfig.h
@@ -262,8 +262,8 @@ extern void calibrateOscillator(void);
  * obdev's free shared VID/PID pair. See the file USB-IDs-for-free.txt for
  * details.
  */
-#define USB_CFG_DEVICE_NAME   'P', 'r', 'o', ' ', 'T', 'r', 'i', 'n', 'k', 'e', 't', ' ', 'M', 'o', 'u', 's', 'e'
-#define USB_CFG_DEVICE_NAME_LEN 17
+#define USB_CFG_DEVICE_NAME   'P', 'r', 'o', ' ', 'T', 'r', 'i', 'n', 'k', 'e', 't', ' ', 'G', 'a', 'm', 'e', 'p', 'a', 'd'
+#define USB_CFG_DEVICE_NAME_LEN 19
 /* Same as above for the device name. If you don't want a device name, undefine
  * the macros. See the file USB-IDs-for-free.txt before you assign a name if
  * you use a shared VID/PID.
@@ -290,7 +290,7 @@ extern void calibrateOscillator(void);
  * HID class is 3, no subclass and protocol required (but may be useful!)
  * CDC class is 2, use subclass 2 and protocol 1 for ACM
  */
-#define USB_CFG_HID_REPORT_DESCRIPTOR_LENGTH    52
+#define USB_CFG_HID_REPORT_DESCRIPTOR_LENGTH    48
 /* Define this to the length of the HID report descriptor, if you implement
  * an HID device. Otherwise don't define it or define it to 0.
  * If you use this define, you must add a PROGMEM character array named
